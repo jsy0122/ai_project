89e0a35a260c5b3c3d5d5fa6b54fd0b244c4a744
jsy0122 <rins4413@gmail.com>
2026-05-12 16:02:36 +0900

    Update main.py

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 import streamlit as st
 st.title('나의 첫 앱 서비스')
-st.text_input( '이름을 입력하세요')
-st.selectbox('좋아하는 음식을 선택하세요!', ['마라탕', '피자', '볶음밥'])
-
+a=st.text_input( '이름을 입력하세요')
+b=st.selectbox('좋아하는 음식을 선택하세요!', ['마라탕', '피자', '볶음밥'])
+if st.buttom('인사말 생성'):
+  st.write(a+'님, 안녕하세요!')
